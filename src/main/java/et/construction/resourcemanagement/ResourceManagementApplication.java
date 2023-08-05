c1c05981b37cd69b4fce6a3de94dc18e35c413de
tare2 <tare2701@gmail.com>
2023-08-05 17:43:54 +0200

    initial commit

---
+++ src/main/java/et/construction/resourcemanagement/ResourceManagementApplication.java
@@ -0,0 +1,13 @@
+package et.construction.resourcemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResourceManagementApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Colleagues" );
+    }
+
+}
